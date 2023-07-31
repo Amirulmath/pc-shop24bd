0826a66eda848be2fbd1455e3ecefd2eb9689d7b
Amirulmath <amirulmath760@gmail.com>
2023-07-31 17:30:54 +0600

    Created all product curd using fake data API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import RootLayout from "@/components/Layouts/RootLayout";
+import AllProducts from "@/components/UI/AllProducts";
 import { Button } from "antd";
 
-const HomePage = () => {
+const HomePage = ({ allProducts }) => {
   return (
     <div>
       <p>Home Page</p>
       <Button type="primary">PC Component</Button>
+      <AllProducts allProducts={allProducts} />
     </div>
   );
 };
@@ -14,4 +16,16 @@ export default HomePage;
 
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/products");
+  const data = await res.json();
+  // console.log(data);
+  return {
+    props: {
+      allProducts: data,
+    },
+    revalidate: 10,
+  };
 };
